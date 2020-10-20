2fb54265a62aef391cba8598c215684753814876
wahaj akmal <44682195+wahaj123@users.noreply.github.com>
2020-10-21 04:35:29 +0500

    eks jenkins terraform

--- main.tf
+++ main.tf
@@ -4,19 +4,3 @@ module "lambda_add" {
   source                   = "./modules/lambda"
   name                     = var.add
 }
-
-module "lambda_delete" {
-  source                   = "./modules/lambda"
-  name                     = var.delete
-}
-
-module "lambda_update" {
-  source                   = "./modules/lambda"
-  name                     = var.update
-}
-
-module "lambda_get" {
-  source                   = "./modules/lambda"
-  name                     = var.get
-}
-
--- variable.tf
+++ variable.tf
@@ -1,12 +1,3 @@
 variable "add"{
     default="add"
 }
-variable "delete"{
-    default="delete"
-}
-variable "update"{
-    default="update"
-}
-variable "get"{
-    default="get"
-}
