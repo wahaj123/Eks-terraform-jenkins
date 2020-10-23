caed664d76985c5f8119256e512944208998c137
wahaj akmal <44682195+wahaj123@users.noreply.github.com>
2020-10-23 22:55:19 +0500

    jenkins file changes

--- Python-app/main.py
+++ Python-app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from Python and wahaj!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
